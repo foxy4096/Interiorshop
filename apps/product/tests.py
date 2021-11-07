0f927461191289c267b1b7e497de2bca6e25dbb1
Aditya Priyadarshi <54215788+foxy4096@users.noreply.github.com>
2021-11-07 20:34:48 +0530

    Added the tests

--- apps/product/tests.py
+++ apps/product/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Product
+
+class ProductTest(TestCase):
+    """
+    Here we'll define the tests that we'll run against our Product model
+    """
+    def setUp(self):
+        Product.objects.create(
+            name='The cathedral and the bazaar',
+            description='A great read',
+            price=25.00,
+        )
+    
+    def test_str(self):
+        product = Product.objects.get(id=1)
+        self.assertEqual(str(product), product.name)
+            
